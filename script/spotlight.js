a2feca46ed6c5e493755facead59125c6951e72b
Daniel B <Dragon863.dev@gmail.com>
2024-12-21 13:39:28 +0000

    Update with changes

--- script/spotlight.js
+++ script/spotlight.js
@@ -1,3 +1,4 @@
+// Used for OnBoard page
 window.addEventListener("pointermove", (event) => {
     document.getElementById("herobox").style.background = `radial-gradient(circle at ${event.pageX}px ${event.pageY}px, rgba(0, 0, 0, 0) 10px, rgba(0, 0, 0, 0.8) 80px)`;
 });
